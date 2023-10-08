99bf6e3b9f6d871a6b5842c62f0e23ad0c4c7318
Mukonazwothe <mukona111@gmail.com>
2023-10-08 09:28:15 -0700

    ARGC AND ARGV

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include “main.h
+
+/**
+*main-print program name
+*@argv: arguments vector
+*@argc: arguments count
+*
+*Return:Always 0 (success)
+*/
+
+int main (int argc _attribute_((unused)), char *argv[])
+{
+        Printf(“%s/n”, *argv);
+
+  return 0;
+}
+
