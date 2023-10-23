785918157373093f5e845a9bcf10554a4bfd4069
Mukonazwothe <mukona111@gmail.com>
2023-10-23 00:59:27 -0700

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,17 @@
 #include "main.h"
+#include <stdlib.h>  // Include the standard library header for malloc, NULL, and exit
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
- *
- * @b: allocated memory
- *
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *i;
+    void *i;
 
-	i = malloc(b);
-	if (i == NULL)
-		exit(98);
-	return (i);
-
+    i = malloc(b);
+    if (i == NULL)
+        exit(98);
+    return (i);
 }
